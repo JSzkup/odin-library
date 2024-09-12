4fb0b40c50df64a882016a3656fbe863ad174581
Jonathan Szkup <jszkup101@gmail.com>
2024-09-12 16:55:46 -0400

    setup and comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
+
+    // TODO place this function in Boko prototype
     this.info = function () {
         if (this.read) {
             strRead = 'already read'
@@ -12,4 +16,16 @@ function Book(title, author, pages, read) {
         }
         return `${title} by ${author}, ${pages} pages, ${strRead}`
     }
+}
+
+// Take user input and store book into array myLibrary
+function addBookToLibrary() {
+    // pull from html fields to create new book objects
+    // put those books in the array
+
+}
+
+// loops through array and displays each book
+function displayBooks() {
+
 }
