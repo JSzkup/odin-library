b9e25555cf65b73440808348c8ad9fb77c3be0dd
Jonathan Szkup <jszkup101@gmail.com>
2024-09-16 18:54:04 -0400

    add html dialog, basic js for pulling the form results

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,9 @@ function Book(title, author, pages, read) {
 // Take user input and store book into array myLibrary
 function addBookToLibrary() {
     // pull from html fields to create new book objects
+
     // put those books in the array
+    myLibrary.push(new Book(title, author, pages, read));
 
 }
 
@@ -29,3 +31,14 @@ function addBookToLibrary() {
 function displayBooks() {
 
 }
+
+// Get the dialog element
+const dialog = document.getElementById('bookDialog');
+
+// Get the button that opens the dialog
+const openDialogBtn = document.getElementById('openDialog');
+
+// Add an event listener to the button to open the dialog
+openDialogBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
