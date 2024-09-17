981fdc069f85612d253df4ad49c04f9108c7d978
Jonathan Szkup <jszkup101@gmail.com>
2024-09-16 21:49:05 -0400

    change html stylesheet location, add id to submit button, move getElementbyId's to top, form functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 const myLibrary = [];
 
+// Get the dialog element
+const dialog = document.getElementById('bookDialog');
+// Get the button that opens the dialog
+const openDialogBtn = document.getElementById('openDialog');
+// form element
+const form = document.getElementById('bookForm');
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -20,7 +27,23 @@ function Book(title, author, pages, read) {
 
 // Take user input and store book into array myLibrary
 function addBookToLibrary() {
+    event.preventDefault();
+
     // pull from html fields to create new book objects
+    const title = document.getElementById('title').value.trim();
+    const author = document.getElementById('author').value.trim();
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+
+    // Display Form Data
+    console.log('Title:', title);
+    console.log('Pages:', pages);
+    console.log('Read:', read);
+    console.log('Author:', author);
+
+    // close dialog and reset the form for new sumissions
+    dialog.close();
+    form.reset();
 
     // put those books in the array
     myLibrary.push(new Book(title, author, pages, read));
@@ -32,13 +55,12 @@ function displayBooks() {
 
 }
 
-// Get the dialog element
-const dialog = document.getElementById('bookDialog');
-
-// Get the button that opens the dialog
-const openDialogBtn = document.getElementById('openDialog');
-
 // Add an event listener to the button to open the dialog
 openDialogBtn.addEventListener('click', () => {
     dialog.showModal();
 });
+
+const submitForm = document.getElementById('submitBtn');
+
+// adds a new book to the library once submitted
+submitForm.addEventListener('click', addBookToLibrary)
